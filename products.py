1319e52554c17e9dc00e7e19d4836a9567f7bf95
Michelle CAO <jiahu0724417@gmail.com>
2020-08-11 16:33:11 +0800

    update new medthod

--- products.py
+++ products.py
@@ -1,12 +1,20 @@
+import os #作業系統模組 operating system
+
 #讀取檔案
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue  #跳過，接著執行下一迴
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+products = [] 
+if os.path.isfile('products.csv'): #檢查檔案是否在
+	print('yes! 有找到喔!') # 有找到開始讀取檔案
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue  #跳過，接著執行下一迴
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案....')
+
 
 # 讓使用者輸入
 while True:
